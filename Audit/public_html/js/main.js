270b46740ecb23ef819c8f69c3d879c981f9a99e
Mr-Fox <41732102+Mr-F0x@users.noreply.github.com>
2018-08-30 11:35:13 +0200

    Initaition

--- Audit/public_html/js/main.js
+++ Audit/public_html/js/main.js
@@ -1,7 +1,2 @@
 $(document).ready(function () {
-    function changeToPassword() {
-        setTimeout(function () {
-            document.getElementById("password").setAttribute("type", "password");
-        }, 500);
-    }
 });
